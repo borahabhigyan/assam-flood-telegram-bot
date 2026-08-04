71e72f39b8169f97bd9c6a550d2f30e6ecb42f59
Abhigyan <61969146+borahabhigyan@users.noreply.github.com>
2026-08-04 10:23:56 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://smartaxom.nesdr.gov.in/analytics/flood/waterlevelinfo"
 
@@ -6,7 +9,12 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-r = requests.get(url, headers=headers, timeout=30)
+r = requests.get(
+    url,
+    headers=headers,
+    timeout=30,
+    verify=False,   # TEMPORARY
+)
 
 print("Status:", r.status_code)
 print("Length:", len(r.text))
